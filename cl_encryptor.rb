d2bbc67094da16c48ffaae4f2a01b6060231b532
cheri allen <cherimarie@gmail.com>
2013-12-18 15:29:41 -0800

    Add decryption option to command line tool

--- cl_encryptor.rb
+++ cl_encryptor.rb
@@ -2,15 +2,24 @@ require './encryptor.rb'
 
 while true 
   @e = Encryptor.new 
-  puts "Enter your secret message, or 'q' to quit:"
-  secret = gets.chomp
+  puts "Enter 'e' to encrypt, 'd' to decrypt, or 'q' to quit:"
+  choice = gets.chomp
 
-  if secret == "q"
+  if choice == "q"
     break
-  else 
+  elsif choice == "e"
+    puts "What's your secret?"
+    secret = gets.chomp
     puts "What rotation should we encrypt it with?"
     rotation = gets.chomp.to_i
     puts @e.encrypt(secret, rotation)
+  elsif choice == "d"
+    puts "What's the ciphertext?"
+    cipher = gets.chomp
+    puts "What rotation was it encrypted with?"
+    rotation = gets.chomp.to_i
+    puts @e.decrypt(cipher, rotation)
+  else 
+    puts "THERE'S ONLY THREE CHOICES."
   end 
-end 
-
+end
